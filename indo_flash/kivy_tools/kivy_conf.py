b8cb3a46dc4a1026a676a16ea215a885a0a04397
magk98 <43295061+magk98@users.noreply.github.com>
2019-04-19 21:59:57 +0200

    WIP :)

--- indo_flash/kivy_tools/kivy_conf.py
+++ indo_flash/kivy_tools/kivy_conf.py
@@ -1,45 +1,28 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
 
+with open("Dictionary1.kv", encoding='utf8') as f:
+    Builder.load_string(f.read())
 
+class LoginScreen(Widget):
+    b_indo = ObjectProperty(None)
+    b_pl = ObjectProperty(None)
 
-
-class LoginScreen(GridLayout):
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 1
-
-        self.inside = BoxLayout()
-        self.inside.cols = 2
-
-        self.inside.add_widget(Label(text="Wybierz język", font_size=20))
-        self.add_widget(self.inside)
-
-        self.button_indo = Button(text='indo', font_size=20)
-        self.button_pl = Button(text='polski', font_size=20)
-        self.button_indo.bind(on_press=self.indo_pressed)
-        self.button_pl.bind(on_press=self.pl_pressed)
-        self.add_widget(self.button_indo)
-        self.add_widget(self.button_pl)
-
-    def indo_pressed(self, instance):
+    def indo_pressed(self):
         lang = "indo"
         print(lang)
 
-    def pl_pressed(self, instance):
+    def pl_pressed(self):
         lang = "pl"
         print(lang)
 
 
-class DictionaryApp(Widget, App):
+class DictionaryApp(App):
     def build(self):
         return LoginScreen()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     DictionaryApp().run()
